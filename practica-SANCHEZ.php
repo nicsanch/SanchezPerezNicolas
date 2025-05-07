f9d6472d8b9a45fc85b29bb4370049b13995fd9c
Nicolás Sánchez Pérez <nico.sanpe@gmail.com>
2025-05-07 16:32:31 +0200

    multiplacion y division

--- practica-SANCHEZ.php
+++ practica-SANCHEZ.php
@@ -2,30 +2,30 @@
 /**
  * Práctica de documentación con phpDocumentor
  * @author Nicolás Sánchez Pérez
- * @version 1.0.0
+ * @version 2.0.0
  */
 
 /**
  * Suma dos números enteros.
  *
  * {@internal No maneja decimales}
- * @param int $a Primer sumando
- * @param int $b Segundo sumando
- * @return int Resultado de la suma
+ * @param int $a Primer numero
+ * @param int $b Segundo numero
+ * @return int Resultado de la multiplicacion
  */
-function sumarEnteros(int $a, int $b): int {
-    return $a + $b;
+function multiplicarEnteros(int $a, int $b): int {
+    return $a * $b;
 }
 
 /**
  * Resta dos números enteros.
  *
- * @param int $a Minuendo
- * @param int $b Sustraendo
- * @return int Resultado de la resta
+ * @param int $a dividendo
+ * @param int $b divisor
+ * @return int Resultado de la multiplicacion
  */
 function restarEnteros(int $a, int $b): int {
-    return $a - $b;
+    return $a / $b;
 }
 
 
